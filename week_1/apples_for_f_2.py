b51c48410f31a224540cbad781c5be79ba0e95fd
Babak Huseynov <babekius@gmail.com>
2021-04-23 22:27:45 +0300

    Week 1. Apples for friends No. 2 task.

---
+++ week_1/apples_for_f_2.py
@@ -0,0 +1,9 @@
+#################################
+#                               #
+#       BABAK HUSEYNOV          #
+#                               #
+#################################
+
+friends = int(input())
+number_of_friends = int(input())
+print(number_of_friends % friends, end='')
