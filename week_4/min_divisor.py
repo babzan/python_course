24ecd11a33c88a2cbd2a21e7fe135d30e33e0cf0
Babak Huseynov <babekius@gmail.com>
2021-04-26 19:36:30 +0300

    Week 4. Minimum divisor.

---
+++ week_4/min_divisor.py
@@ -0,0 +1,14 @@
+
+
+def MinDivisor(n):
+    i = 2
+    while n % i != 0:
+        if n ** 0.5 == i:
+            i += 1
+        else:
+            return n
+    return i
+
+n = int(input())
+
+print(MinDivisor(n))
