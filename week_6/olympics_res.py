f85f0237d8b1cb270a99a81ea238fb5fac3b2f82
Babak Huseynov <babekius@gmail.com>
2021-05-08 20:41:10 +0300

    Week 6. Olympics + points for university.

---
+++ week_6/olympics_res.py
@@ -0,0 +1,9 @@
+
+
+count = int(input())
+members = []
+for i in range(count):
+    members.append(list(input().split()))
+members.sort(key=lambda ls: int(ls[1]), reverse=True)
+for member in members:
+    print(member[0])
