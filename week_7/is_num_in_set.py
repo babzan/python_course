d90409831fa1f4f9e334497a74d5ffc8995dac73
Babak Huseynov <babekius@gmail.com>
2021-05-08 20:52:43 +0300

    Week 7. Is number in set.

--- week_7/is_num_in_set.py
+++ week_7/is_num_in_set.py
@@ -4,3 +4,12 @@
 #                               #
 #################################
 
+set_1 = list(map(int, input().split()))
+set_2 = set()
+
+for i in range(len(set_1)):
+    if set_1[i] not in set_2:
+        set_2.add(set_1[i])
+        print('NO')
+    else:
+        print('YES')
