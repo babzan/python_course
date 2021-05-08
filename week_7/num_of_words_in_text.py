91e767195286cfb0ee2e7f6abd9e113ee8eafcd9
Babak Huseynov <babekius@gmail.com>
2021-05-08 20:55:23 +0300

    Week 7. Number of words in text.

--- week_7/num_of_words_in_text.py
+++ week_7/num_of_words_in_text.py
@@ -4,3 +4,9 @@
 #                               #
 #################################
 
+# For test it you'll need use this command at command
+# line in UNIX-like systems:
+# cat text_file.txt | python3 python.py
+
+import sys
+print(len(set(sys.stdin.read().split())))
