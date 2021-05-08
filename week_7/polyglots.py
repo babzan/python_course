6a526084a78e61e18b19bca7d7d96124e2238aec
Babak Huseynov <babekius@gmail.com>
2021-05-08 20:54:45 +0300

    Week 7. Polyglots.

--- week_7/polyglots.py
+++ week_7/polyglots.py
@@ -4,3 +4,17 @@
 #                               #
 #################################
 
+total_langs, all_langs = set(), set()
+for i in range(int(input())):
+    langs = set(input() for _ in range(int(input())))
+    total_langs |= langs
+    all_langs = all_langs & langs if i else langs
+print(len(all_langs), '\n'.join(all_langs), sep='\n')
+print(len(total_langs), '\n'.join(total_langs), sep='\n')
+
+seq = map(int, input().split())
+countDict = {}
+for elem in seq:
+    countDict[elem] = countDict.get(elem, 0) + 1
+for key in sorted(countDict):
+    print(key, countDict[key], sep=' : ')
